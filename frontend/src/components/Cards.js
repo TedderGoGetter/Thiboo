36de517842633310263992257fbf3aaa20c1849d
TedderGoGetter <103417758+TedderGoGetter@users.noreply.github.com>
2022-07-03 20:14:24 +0900

    embeds inserted

--- frontend/src/components/Cards.js
+++ frontend/src/components/Cards.js
@@ -1,7 +1,16 @@
+import YoutubeEmbed from './YoutubeEmbed'
+import './CardsNYT.css'
+
+
 function Cards() {
 
     return(
         <div className="cardstack">
+            <YoutubeEmbed className="embeds" embedId="OGV5rJ40r4w" />
+            <YoutubeEmbed className="embeds" embedId="w19wmJCgGsc" />
+            <YoutubeEmbed className="embeds" embedId="JDRad1L-Iuk" />
+            <YoutubeEmbed className="embeds" embedId="hYd-hJ3Khyk" />
+            <YoutubeEmbed className="embeds" embedId="0BHSS8tL1b4" />
             
 
         </div>
---
+++ frontend/src/components/YoutubeEmbed.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types"
+import './CardsNYT.css'
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="640"
+      height="385"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+
+export default YoutubeEmbed
