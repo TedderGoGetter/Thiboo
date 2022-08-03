e3632d4d693083e3c7b87720459f569b99a21340
TedderGoGetter <103417758+TedderGoGetter@users.noreply.github.com>
2022-08-03 15:05:03 +0900

    Hamburger and logo made responsive

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,20 +1,41 @@
 import './NavBar.scss'
+import {useState} from 'react'
 
 const NavBar = ({aboutClick, contactClick}) => {
+
+    const burg = new URL('../images/hamburgerIvory.png', import.meta.url)
+    const burgX = new URL('../images/hamburgerClosedIvory.png', import.meta.url)
+
+    const [currentClass, setCurrentClass] = useState('burgOpen')
+
+    const burgClick = () => {
+        if (currentClass === 'burgOpen') setCurrentClass('burgClosed')
+        if (currentClass === 'burgClosed') setCurrentClass('burgOpen')
+    }
 
 
     return (
         <div className="navBar">
-            <div className="navContent">
-                <a className="abtTag" href="/" target="_blank" onClick={
-                        (e) => aboutClick(e)
-                        }><h3 className='abt' >ABOUT</h3>
-                </a>
+            
+            <div className={currentClass} onClick={burgClick}>
+                <div>
+                    <img className="burgPic" src={burg} alt="="/>
+                    <img className="burgXPic" src={burgX} alt="x"/>  
+                </div>
 
-                <a className="abtTag" href="/" target="_blank" onClick={
-                        (e) => contactClick(e)
-                    }><h3 className='abt' >CONTACT</h3>
-                </a>
+                <div className="navContent">
+                    <div className="spaceMaker"></div>
+
+                    <a className="abtTag" href="/" target="_blank" onClick={
+                            (e) => aboutClick(e)
+                            }><h3 className='abt' >ABOUT</h3>
+                    </a>
+
+                    <a className="abtTag" href="/" target="_blank" onClick={
+                            (e) => contactClick(e)
+                        }><h3 className='cntct' >CONTACT</h3>
+                    </a>
+                </div>
             </div>
         </div>
     )
